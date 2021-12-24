629d6e34a10d3e27bba28c1bb46cd120845b3b7c
Oleksandr Koval <lexkoval92@gmail.com>
2021-12-24 12:26:05 +0200

    fix: project local path

---
+++ src/components/Button.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = ({text, isEnable}) => {
+  return (
+    <ButtonStyled disabled={isEnable}>
+      {text}
+    </ButtonStyled>
+  );
+};
+
+const ButtonStyled = styled.button`
+  min-width: 200px;
+  width: 260px;
+  display: inline-block;
+  text-align: center;
+  background-color: #1b2235;
+  color: #fff;
+  text-decoration: none;
+  padding: 18px 20px;
+  font-size: 16px;
+  font-weight: 500;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -.05px;
+  cursor: pointer;
+  border: none;
+  border-radius: 8px;
+  -webkit-box-shadow: 0 11px 19px 0 rgb(0 0 0 / 16%);
+  box-shadow: 0 11px 19px 0 rgb(0 0 0 / 16%);
+  
+  &:disabled {
+    color: hsla(0,0%,100%,.3);
+    cursor: default;  }
+`;
+
+export default Button;
